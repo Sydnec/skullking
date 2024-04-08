e02ce5aa930324028abf42b29d347a78927f1c08
Sydnec <simon.bourlier@etu.mines-ales.fr>
2024-04-08 14:45:12 +0200

    WIP Room list CSS

--- myapp/views/home.py
+++ myapp/views/home.py
@@ -1,9 +1,17 @@
 # myapp/views/home.py
 
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from myapp.models.models import Room, Player
 
 def display_home(request):
     return render(request, 'myapp/home.html')
 
 def display_rooms(request):
-    return render(request, 'myapp/rooms.html')
+    rooms = Room.objects.exclude(rounds__isnull=False)
+    room_data = []
+    for room in rooms:
+        players = room.players.all()
+        room_usernames = [player.user.username for player in players]
+        room_data.append({'code': room.code, 'usernames': room_usernames})
+    return render(request, 'myapp/rooms.html', {'rooms_data': room_data})
